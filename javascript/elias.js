ab9c6af3934b174582464ecd59739a239957db43
Braian-de-Liz <braian_l_silva@estudante.sesisenai.org.br>
2025-07-03 14:33:49 -0300

    Mario Funcional

--- javascript/elias.js
+++ javascript/elias.js
@@ -2,63 +2,61 @@ const quemario = document.querySelector('.QUE-MÁRIO');
 const tubo = document.querySelector('.tubo');
 
 let loop;
-
+let isAlive = true;
 
 function pulo() {
+    if (!isAlive) return;
     quemario.classList.remove('pulo');
     void quemario.offsetWidth; 
     quemario.classList.add('pulo');
 }
 
-
 function iniciarLoop() {
-    console.log("Iniciando loop...");
+    console.log("Loop iniciado");
 
     loop = setInterval(() => {
         const posicaoTubo = tubo.offsetLeft;
         const posicaoMario = +window.getComputedStyle(quemario).bottom.replace('px', '');
 
         if (posicaoTubo <= 120 && posicaoTubo > 0 && posicaoMario < 80) {
+            console.log("COLISÃO!");
 
             tubo.style.animation = 'none';
             tubo.style.left = `${posicaoTubo}px`;
 
-            quemario.style.animation = 'none';
-            quemario.style.bottom = `${posicaoMario}px`;
             quemario.src = '../ASSETS/imagens/mario_morre.png';
             quemario.style.width = '75px';
+            quemario.style.animation = 'none';
+            quemario.style.bottom = `${posicaoMario}px`;
 
             clearInterval(loop);
+            isAlive = false;
 
-            setTimeout(reiniciarJogo, 1000);
+            setTimeout(reiniciarJogo, 1200);
         }
     }, 10);
 }
 
-
 function reiniciarJogo() {
-    console.log("Reiniciando o jogo...");
+    console.log("Reiniciando...");
 
     tubo.style.animation = 'none';
-    tubo.offsetWidth; 
+    tubo.offsetWidth;
     tubo.style.animation = 'animacao-tubo 2s infinite linear';
-    tubo.style.left = "";
+    tubo.style.left = '';
 
-    quemario.style.animation = 'none';
-    quemario.offsetWidth;
-    quemario.style.bottom = "0px";
+    quemario.src = '../ASSETS/imagens/mario_atras_armario.gif?t=' + new Date().getTime();
+    quemario.style.width = '100px';
+    quemario.classList.remove('pulo');
+    quemario.style.animation = '';
+    quemario.style.bottom = '0px';
 
-
-    quemario.src = '../ASSETS/imagens/mario_atras_armario.gif';
-    quemario.style.width = ''; 
-
-    quemario.onload = () => {
+    isAlive = true;
+    setTimeout(() => {
         iniciarLoop();
-    };
+    }, 100);
 }
 
-
 document.addEventListener('keydown', pulo);
-
 
 iniciarLoop();
